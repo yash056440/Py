de07a51eafa38c13a94502f4b09d638e279d7b37
yash1 <yash056440@gmail.com>
2025-11-19 09:52:49 +0530

    print_livematch_team name

--- base_1_cri.py
+++ base_1_cri.py
@@ -1,11 +1,14 @@
 livescore = 'https://www.cricbuzz.com/cricket-match/live-scores'
-# from package-name import module name
+
 from bs4 import BeautifulSoup
 import requests
 
 req = requests.get(livescore)
 soup = BeautifulSoup(req.text, 'html.parser')
-imptext = soup.find('div',class_='flex flex-col gap-3 my-2  ')
-for data in imptext.find_all("span",class_= "text-cbTxtSec"):
-    print(data.text)
-print('_'*100)
+imptext = soup.find('div',class_='flex flex-col gap-3 my-2')
+
+for data1 in imptext.find_all("span",class_='text-cbTxtPrim dark:text-cbWhite block wb:hidden whitespace-nowrap'):   
+    for data2 in imptext.find_all("span",class_='text-cbTxtSec dark:text-cbTxtSec block wb:hidden whitespace-nowrap'):
+        print(data2.text,"Vs",data1.text)
+    
+print('_'*10)
